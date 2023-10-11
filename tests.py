aa307faa0d1455a53eefd2a674e2872c08c5cbd4
Savelyev Egor <savelyev.eg@gmail.com>
2023-10-12 02:58:31 +0700

    test db select and return value

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ class LinesTestCase(unittest.TestCase):
                 '6s26p', '2P°', '3/2', '6s6p2(3P)', '4P', '1/2', 4, 2, ' ',
                  'T10236', 'L9843.L21488.L3331'
                 )
-        line2 = Line('O I', 500.1, 500.1, 140, 3000000, 'E', 20788.18, 70253.71, 
+        line2 = Line('O I', 500.1, 500.1, 1000, 3000000, 'E', 20788.18, 70253.71, 
                 '6s26p', '2P°', '3/2', '6s6p2(3P)', '4P', '1/2', 4, 2, ' ',
                  'T10236', 'L9843.L21488.L3331'
                 )
@@ -33,9 +33,24 @@ class LinesTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_home(self):
+    def test_status_code_200(self):
         rv = self.client.get('/lines/')
         self.assertEqual(rv.status_code, 200)
+    
+    def test_db_select(self):
+        with self.app.app_context():
+            self.assertEqual(Line.query.count(), 3)
+            self.assertEqual(Line.query.get(1).ion, 'Bi III')
+            res = Line.query.filter(Line.rel_int > 100).all()
+            self.assertEqual(len(res), 2)
+            self.assertGreater(res[0].rel_int, 100)
+            self.assertGreater(res[1].rel_int, 100)
+
+    def test_return_value(self):
+        rv = self.client.get('/lines/')
+        self.assertEqual(rv.json[0]['wavelength'], 202.0958)
+        self.assertEqual(rv.json[1]['ion'], 'O I')
+
 
 if __name__ == '__main__':
     unittest.main()
